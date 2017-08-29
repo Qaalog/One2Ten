fc33934380ee1b0e7465f96d191df1bdce412642
Oleg Lukashov <olukashov@itera-research.com>
2017-08-29 19:06:52 +0300

    merge config with server environment

--- config.php
+++ config.php
@@ -1,13 +1,19 @@
 <?php
 
+$envDbServer = getenv("webservice_db_server");
+$envDbName   = getenv("webservice_db_name");
+$envDbUser   = getenv("webservice_db_user");
+$envDbPassword = getenv("webservice_db_password");
+$envWebserviceUrl = getenv("webservice_tol_ws_url");
+
 $config = array(
-    'server_name'   => 'toldb.database.windows.net',
-    'database_name' => 'one2ten',  //'one2ten',
-    'user_name'     => 'o2t',
-    'user_password' => 'L##3v3r4g3',
+    'server_name'   => empty($envDbServer) ? 'toldb-dev.database.windows.net' : $envDbServer,
+    'database_name' => empty($envDbName)   ? 'one2ten_dev' : $envDbName,
+    'user_name'     => empty($envDbUser)   ? 'ptadeu' : $envDbUser,
+    'user_password' => empty($envDbPassword) ? 'L%%3v3r4g3' : $envDbPassword,
     'driver_type'   => 'sqlsrv',
     //
     //'webservice_url' => 'http://teamol.co/testap/',
-    'webservice_url' => 'http://tolws-dev.azurewebsites.net/',
+    'webservice_url' => empty($envWebserviceUrl) ? 'http://tolws-dev.azurewebsites.net/' : $envWebserviceUrl,
 );
 
